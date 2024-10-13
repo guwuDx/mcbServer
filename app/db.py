7d1419fba6ab3d0816978b3faeae3a989c78ed59
guwudx <2795008637@qq.com>
2024-10-13 18:52:56 +0800

    cnf adj

--- app/db.py
+++ app/db.py
@@ -9,15 +9,22 @@ def get_db_connection():
         pass
     else:
         raise Exception("Unsupported database type")
+    
+    if not db_config.get("user"):
+        raise Exception("Database user not specified")
+    if not db_config.get("password"):
+        raise Exception("Database password not specified")
+    if not db_config.get("database"):
+        raise Exception("Database name not specified")
 
     return mysql.connector.connect(
-        host=db_config["host"],
-        port=db_config["port"],
+        host=db_config.get("host", "localhost"),
+        port=db_config.get("port", 3306),
+        charset=db_config.get("charset", "utf8mb4"),
+        collation=db_config.get("collation", "utf8_general_ci"),
         user=db_config["user"],
         password=db_config["password"],
-        database=db_config["database"],
-        charset=db_config["charset"],
-        collation=db_config["collation"]
+        database=db_config["database"]
     )
 
 
@@ -38,6 +45,16 @@ def query_header_gen(shape, shapeInfo):
 
 def query_generic_gen(genericSet):
     sql_pt_generic = ""
+
+    for generic in genericSet:
+        if sql_pt_generic:
+            if generic['logic'] == 1:
+                sql_pt_generic += "AND "
+            elif generic['logic'] == 2:
+                sql_pt_generic += "OR "
+
+        sql_pt_generic
+
     return sql_pt_generic
 
 
