ec6445cbb6b715040ebc9233fe7569530cecd45b
guwudx <2795008637@qq.com>
2024-10-30 17:24:32 +0800

    modify the project stucture, test pass

---
+++ app/schemas.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel, EmailStr
+from typing import List, Optional
+
+
+class Shape(BaseModel):
+    id: int
+    name: str
+
+class Generic(BaseModel):
+    logic: int
+    parameter: int
+    rangeMode: int
+    showRange: bool
+    rangeStart: Optional[float]
+    rangeEnd: Optional[float]
+    value: Optional[float]
+    dispSwitch: int
+    selectedMaterial: Optional[int]
+    isInvert: bool
+
+class Freq(BaseModel):
+    logic: int
+    parameter: int
+    rangeMode: int
+    showRange: bool
+    rangeStart: Optional[float]
+    rangeEnd: Optional[float]
+    value: Optional[float]
+    dispSwitch: int
+    selectedSparam: Optional[int]
+    isInvert: bool
+
+class queryRequest(BaseModel):
+    shapeSet: List[Shape]
+    genericSet: List[Generic]
+    freqSet: List[Freq]
